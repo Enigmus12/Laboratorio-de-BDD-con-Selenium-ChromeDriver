593edc520c7665e1b3880d9c7ac72e9ef5afbb20
Juan <Jdrorodriguez2@gmail.com>
2026-03-07 21:51:08 -0500

    Lab3

---
+++ bdd-java/src/main/java/com/eci/myproject/App.java
@@ -0,0 +1,7 @@
+package com.eci.myproject;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("BDD Java project initialized.");
+    }
+}
---
+++ bdd-java/src/test/java/com/eci/myproject/AppTest.java
@@ -0,0 +1,11 @@
+package com.eci.myproject;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void shouldPass() {
+        Assert.assertTrue(true);
+    }
+}
